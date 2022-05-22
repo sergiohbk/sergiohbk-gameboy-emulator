2420490ee5607765bba26e848e05c5a7ff0551f3
sergiohbk <sergiohbk92@gmail.com>
2022-05-22 06:00:15 +0200

    parte de la screen completada (a editar tamaños)

--- src/UI/Gameboy-screen.js
+++ src/UI/Gameboy-screen.js
@@ -1,12 +1,40 @@
 import './Gameboy-screen.css';
 
-export function GameboyScreen(){
+
+function Screenbattery(){
     return(
-        <div className="container-screen">
-            <div className="screen">
+        <div className="screen-battery">
+            <div className="led">
+            </div>
+            <div className="screen-battery-text">
+                BATTERY
+            </div>
+        </div>
+    );
+}
+
+function Screen(){
+    return(
+        <div className="screen">
+            <div className="screen-top">
+                <div className="screen-top-stripes">
+                </div>
+                <span>DOT MATRIX WITH STEREO SOUND</span>
+            </div>
+            <div className="screen-bottom">
+                <Screenbattery />
                 <div className="lcd">
                 </div>
             </div>
+        </div>
+    );
+    //sustituir div por canvas
+}
+
+export function GameboyScreen(){
+    return(
+        <div className="container-screen">
+            <Screen />
             <div className="nintendo">
                 <span className="nintendo-logo">Nintendo </span>
                 <span className="model">GAME BOY</span>
--- src/UI/Gameboy-ui.js
+++ src/UI/Gameboy-ui.js
@@ -2,6 +2,13 @@ import { GameboyControls } from "./Gameboy-controls";
 import { GameboyScreen } from "./Gameboy-screen";
 import { GameboyTop } from "./Gameboy-top";
 
+/*///////////////////////
+/////////////////////////
+////Gameboy  Emulator////
+/////By Sergio-hbk///////
+/////////////////////////
+////////////////////////*/
+
 export function GameboyConsole(){
     return(
         <div className="container-gb">
