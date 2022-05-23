5d56452919122aa3a9e99729aaa524dfaebb4e7f
sergiohbk <sergiohbk92@gmail.com>
2022-05-23 06:21:07 +0200

    añadidos diseño de pantalla, led, y estructura

--- src/UI/Gameboy-controls.js
+++ src/UI/Gameboy-controls.js
@@ -1,8 +1,48 @@
 import './Gameboy-controls.css';
 
+function Dpad(){
+    return(
+        <div className="D-pad">
+        </div>
+    );
+}
+
+function Button(props){
+    return(
+        <button className="button">
+            {props.name}
+        </button>
+    );
+}
+
+function Speakers(){
+    return(
+        <div className="speakers">
+        </div>
+    );
+}
+
 export function GameboyControls (){
     return(
         <div className="container-controls">
+            <div className="controls">
+                <Dpad />
+                <div className="buttons">
+                    <Button name="B" />
+                    <Button name="A" />
+                </div>
+            </div>
+            <Speakers />
+            <div className="option-buttons">
+                <Button name="SELECT" />
+                <Button name="START" />
+            </div>
+            <div className="bottom">
+                <div className="phones">
+                </div>
+                <div className="decoration">
+                </div>
+            </div>
         </div>
     );
 }
